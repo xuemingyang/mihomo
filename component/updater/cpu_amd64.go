dfe6e0509b0db9e969d82d3e031dd044b4fd5ea8
wwqgtxx <wwqgtxx@gmail.com>
2025-07-24 02:06:50 +0800

    chore: rebuild core updater

--- component/updater/cpu_amd64.go
+++
@@ -1,4 +0,0 @@
-package updater
-
-// getGOAMD64level is implemented in cpu_amd64.s. Returns number in [1,4].
-func getGOAMD64level() int32
--- component/updater/cpu_others.go
+++
@@ -1,8 +0,0 @@
-//go:build !amd64
-
-package updater
-
-// getGOAMD64level is always return 0 when not in amd64 platfrom.
-func getGOAMD64level() int32 {
-	return 0
-}
