b4c3bbf660f326dd1918d003d87d6dd82d99038c
wwqgtxx <wwqgtxx@gmail.com>
2025-08-12 20:06:08 +0800

    chore: sync vless encryption code

--- transport/vless/encryption/doc.go
+++ transport/vless/encryption/doc.go
@@ -2,4 +2,5 @@
 // https://github.com/XTLS/Xray-core/commit/f61c14e9c63dc41a8a09135db3aea337974f3f37
 // https://github.com/XTLS/Xray-core/commit/3e19bf9233bdd9bafc073a71c65b737cc1ffba5e
 // https://github.com/XTLS/Xray-core/commit/7ffb555fc8ec51bd1e3e60f26f1d6957984dba80
+// https://github.com/XTLS/Xray-core/commit/ec1cc35188c1a5f38a2ff75e88b5d043ffdc59da
 package encryption
